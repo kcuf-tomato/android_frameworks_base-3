8398a636f3887d60d7da42a78e8edfe475950ff9
vjspranav <pranavasri@live.in>
2021-05-18 14:41:46 +0200

    Revert "Refactor how FOD Animation is hidden"
    This reverts commit d0f3d5f2efd3eea4fa1d46990faf3b756eb848d0.

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -473,7 +473,9 @@ public class FODCircleView extends ImageView {
 
         if (event.getAction() == MotionEvent.ACTION_DOWN && newIsInside) {
             showCircle();
-            mHandler.post(() -> mFODAnimation.showFODanimation());
+            if (mIsRecognizingAnimEnabled) {
+                mHandler.post(() -> mFODAnimation.showFODanimation());
+            }
             return true;
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
             hideCircle();
@@ -628,9 +630,8 @@ public class FODCircleView extends ImageView {
     private void updateStyle() {
         mIsRecognizingAnimEnabled = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.FOD_RECOGNIZING_ANIMATION, 0) != 0;
-
         if (mFODAnimation != null) {
-            mFODAnimation.update(mIsRecognizingAnimEnabled);
+            mFODAnimation.update();
         }
     }
 
