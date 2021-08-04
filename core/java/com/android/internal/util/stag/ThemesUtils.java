TenX-OS/frameworks_base

SystemUI: Introduce Custom Statusbar Signal & Wi-Fi icons [1/3]
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

core/java/com/android/internal/util/tenx/ThemesUtils.java
@@ -127,6 +127,20 @@ public class ThemesUtils {
         "com.android.system.switch.telegram", // 6
     };
 
+    // Statusbar Signal icons
+    private static final String[] SIGNAL_BAR = {
+        "com.tenx.systemui.signalbar_a",
+        "com.tenx.systemui.signalbar_b",
+        "com.tenx.systemui.signalbar_c",
+    };
+
+    // Statusbar Wifi icons
+    private static final String[] WIFI_BAR = {
+        "com.tenx.systemui.wifibar_a",
+        "com.tenx.systemui.wifibar_b",
+        "com.tenx.systemui.wifibar_c",
+    };
+
     public String[] getTheme(int theme) {
         switch (theme) {
             case DEVICE_THEME_LIGHT:

kcuf-tomato/android_frameworks_base-3

Signed-off-by: Jackeagle <jackeagle102@gmail.com>
Change-Id: I935c7c03fe8e0fb2fb4b9b71f34bc34ae04a5706
Signed-off-by: Cruzer1 <preetamswain143@gmail.com>

core/java/com/android/internal/util/stag/ThemesUtils.java
@@ -35,7 +35,7 @@ public class ThemesUtils {
     private static final String[] SWITCH_THEMES = {
         "com.android.system.switch.stock", // 0
         "com.android.system.switch.oneplus", // 1
-	"com.android.system.switch.narrow", // 2
+	    "com.android.system.switch.narrow", // 2
         "com.android.system.switch.contained", // 3
         "com.android.system.switch.telegram", // 4
         "com.android.system.switch.md2", // 5
@@ -45,6 +45,20 @@ public class ThemesUtils {
         "com.android.system.switch.oos", // 9
         "com.android.system.switch.fluid", // 10
         "com.android.system.switch.android_s", // 11
+    };
+    
+    // Statusbar Signal icons
+    private static final String[] SIGNAL_BAR = {
+        "org.blissroms.systemui.signalbar_a",
+        "org.blissroms.systemui.signalbar_b",
+        "org.blissroms.systemui.signalbar_c",
+    };
+
+    // Statusbar Wifi icons
+    private static final String[] WIFI_BAR = {
+        "org.blissroms.systemui.wifibar_a",
+        "org.blissroms.systemui.wifibar_b",
+        "org.blissroms.systemui.wifibar_c",
     };
 
     public static void updateSwitchStyle(IOverlayManager om, int userId, int switchStyle) {